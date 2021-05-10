74e759fe0c462c3cc870f1308d3e1a181c224c27
mattd-silva22 <78603546+mattd-silva22@users.noreply.github.com>
2021-05-10 16:37:33 -0300

    Add files via upload

---
+++ projeto-CodeCamp64/js/script_menu.js
@@ -0,0 +1,13 @@
+let hamBtn = document.querySelector(".hamburger") 
+
+
+ hamBtn.addEventListener( "click" , e => {
+     hamBtn.classList.toggle("is-active")
+    //abrir menus 
+
+
+    console.log(hamBtn.classList)
+})
+console.log(hamBtn)
+console.log(hamBtn.classList)
+
